ff3cd4cf22e034e7da49546f3db33f9906401fc5
David Fardoe <dfardoe@winnipeg.ca>
2019-02-12 16:11:17 -0600

    Added FormArray

--- ng-reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ ng-reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -15,7 +15,10 @@ export class ProfileEditorComponent implements OnInit {
       city: [''],
       province: [''],
       postalCode: ['']
-    })
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
   constructor(private fb: FormBuilder) { }
@@ -36,4 +39,11 @@ export class ProfileEditorComponent implements OnInit {
     });
   }
 
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
 }
