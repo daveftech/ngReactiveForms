554880ed60d096959cd80ff2fd085bd47b5f87b6
David Fardoe <dfardoe@winnipeg.ca>
2019-02-12 15:50:47 -0600

    Added form controls using FormControl and FormGroup

---
+++ ng-reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.scss']
+})
+export class ProfileEditorComponent implements OnInit {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      province: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Dave',
+      address: {
+        street: '405 Assiniboine'
+      }
+    });
+  }
+
+}
